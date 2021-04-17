3c6ea20ebfd89aaf4f9f5710c28c44d85139aa04
Ritvik <ritvik.sardana11@gmail.com>
2021-04-18 00:04:14 +0530

    Started JS

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-//variable
+//variables
+let addBtn = document.querySelector("#addBtn");
+let addTxt = document.querySelector("#addTxt");
 
 
 //event listeners
+//If users adds a note, add it to local storage
+addBtn.addEventListener("click",putData);
+
 
 //functions
+
+function putData(e){
+    let notes = localStorage.getItem("notes");
+
+}
