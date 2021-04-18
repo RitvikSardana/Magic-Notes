c4d80a5a6fbaa9a20a5ae21a100d9d54c0b15fe8
Ritvik <ritvik.sardana11@gmail.com>
2021-04-18 13:53:25 +0530

    ALL done

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 let addBtn = document.querySelector("#addBtn");
 let addTxt = document.querySelector("#addTxt");
 let notesElement = document.querySelector("#notes");
+let searchTxt = document.querySelector("#searchTxt");
+let noteCards = document.querySelector(".noteCards");
+
 showNotes();
 //event listeners
 //If users adds a note, add it to local storage
 addBtn.addEventListener("click",putData);
+searchTxt.addEventListener("input",search)
 
 
 //functions
@@ -37,7 +41,7 @@ function showNotes(){
 
     notesObj.forEach(function(element,index) {
         html += `<div class="card mx-2 my-2" style="width: 18rem">
-        <div class="card-body">
+        <div class="note">
             <h5 class="card-title">Note ${index+1}</h5>
             <p class="card-text">
             ${element}
@@ -60,4 +64,36 @@ function showNotes(){
 
 
 // Function to delete a note
+function deleteNote(index) {
+    console.log("Deleting note ",index);
 
+
+    let notes = localStorage.getItem("notes");
+    if(notes ==null) {
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.splice(index,1);
+    localStorage.setItem("notes",JSON.stringify(notesObj));
+    showNotes();
+}
+
+function search() {
+    let inputVal = searchTxt.value.toLowerCase();
+    let t = noteCards.getElementsByClassName("card"); // to get class of the note element
+    let allNotes = noteCards.getElementsByClassName("card-text");
+    console.log("triggered ",inputVal);
+    for(i=0;i<allNotes.length;i++){
+        let cardTxt = allNotes[i].innerText;
+        let t = noteCards.getElementsByClassName("card")
+        if(cardTxt.includes(inputVal)){
+            t[i].style.display = "block";
+        }
+        else{
+            t[i].style.display = "none"
+        }
+    }
+}
